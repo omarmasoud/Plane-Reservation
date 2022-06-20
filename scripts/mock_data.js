034562d9e3ef65bfc464e936c3fadc240f4edf1c
“Muhammad <modogogo2000@hotmail.com>
2022-06-20 14:24:28 +0200

    Completed Dashboard Added flight suggestions in dashboard

---
+++ scripts/mock_data.js
@@ -0,0 +1,45 @@
+const airports = ["CAI", "LAX", "SAU", "ALE"];
+const suggested_destinations = [
+    {
+        background: "https://i.insider.com/5d26280921a86107bb51bd92?width=700",
+        country: "Japan",
+        city: "Tokyo",
+        price: "9,963",
+        airport: "HND",
+    },
+    {
+        background: "https://lp-cms-production.imgix.net/2021-09/The%20Acropolis%20and%20downtown%20Athens.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850",
+        country: "Greece",
+        city: "Athens",
+        price: "4,519",
+        airport: "ATH",
+    },
+    {
+        background: "https://static.dw.com/image/60208186_303.jpg",
+        country: "France",
+        city: "Paris",
+        price: "7,369",
+        airport: "CDG",
+    },
+    {
+        background: "https://www.planete-energies.com/sites/default/files/styles/media_full_width_940px/public/thumbnails/image/moscow.jpg?itok=J_vRU4rA",
+        country: "Russia",
+        city: "Moscow",
+        price: "6,636",
+        airport: "SVO",
+    },
+    {
+        background: "https://media.tacdn.com/media/attractions-splice-spp-674x446/0c/0d/cb/21.jpg",
+        country: "Egypt",
+        city: "Cairo",
+        price: "2,957",
+        airport: "CAI",
+    },
+    {
+        background: "https://jakadatoursegypt.com/wp-content/uploads/2020/12/Alexandria-Egypt.jpg",
+        country: "Egypt",
+        city: "Alexandria",
+        price: "2,975",
+        airport: "HBE",
+    },
+]
